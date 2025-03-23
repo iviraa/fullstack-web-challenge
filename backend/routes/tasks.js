133ea5993ce9c29ef9b104d26b98cd47b1b8d402
iviraa <chetanchal.cs@gmail.com>
2025-03-23 03:42:40 -0500

    Implement CRUD endpoints for todos

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,3 +3,65 @@ const auth = require("../middleware/auth");
 const Task = require("../models/Task");
 
 const router = express.Router();
+
+// Get all user tasks : GET /api/tasks
+router.get("/", auth, async (req, res) => {
+  try {
+    const tasks = await Task.find({ userId: req.user.id });
+    res.json(tasks);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Add new task : POST /api/tasks
+router.post("/", auth, async (req, res) => {
+  const { title } = req.body;
+  try {
+    const task = new Task({
+      userId: req.user.id,
+      title,
+    });
+    await task.save();
+    res.json(task);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Update task : PUT /api/tasks/:id
+router.put("/:id", auth, async (req, res) => {
+  const { title, completed } = req.body;
+  try {
+    let task = await Task.findOne({ _id: req.params.id, userId: req.user.id });
+    if (!task) return res.status(404).json({ msg: "Task not found" });
+
+    task.title = title !== undefined ? title : task.title;
+    task.completed = completed !== undefined ? completed : task.completed;
+
+    task = await task.save();
+    res.json(task);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Delete task : DELETE /api/tasks/:id
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndRemove({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+    if (!task) return res.status(404).json({ msg: "Task not found" });
+    res.json({ msg: "Task removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
