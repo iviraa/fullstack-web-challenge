0d8dfbdf1b17e068e74f76461cff90d95456f390
iviraa <chetanchal.cs@gmail.com>
2025-03-23 03:12:36 -0500

    Implement JWT auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(401).json({ msg: "Authorization denied" });
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: "Invalid token" });
+  }
+};
+
+module.exports = auth;
