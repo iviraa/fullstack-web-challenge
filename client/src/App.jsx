361cf64edc989617c2fd54e0882c07dc9120d028
iviraa <chetanchal.cs@gmail.com>
2025-03-23 06:20:42 -0500

    Setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Todo</h1>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
